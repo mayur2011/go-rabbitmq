6702b97d775d3b8989d438122b749d96f98ef688
mayur2011 <er.mayur2011@gmail.com>
2022-08-20 02:05:28 +0530

    rmq initial

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
 func main(){
 	fmt.Println("rmq implementation")
 	// try to connect to rabbitmq
-
+	rabbitConn, err := connect()
+	if err !=nil{
+		log.Println(err)
+		os.Exit(1)
+	}
+	defer rabbitConn.Close()
 
 	// start listening for messages
 
@@ -12,4 +21,34 @@ func main(){
 
 
 	// watch the queue and consume events from the topic
+}
+
+func connect() (*amqp.Connection, error){
+	var counts int64
+	var backoff = 1*time.Second
+	var connection *amqp.Connection
+
+	// don't continue until rabbit is ready
+	for {
+		c, err := amqp.Dial("amqp://guest:guest@localhost")
+		if err != nil{
+			panic(err)
+			counts++
+		}else{
+			connection = c
+		}
+
+		if counts > 5{
+			fmt.Println(err)
+			return nil, err
+		}
+
+		// 
+		backoff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
+		log.Println("backing off...")
+		time.Sleep(backoff)
+		continue
+	}
+
+	return connection, nil
 }
