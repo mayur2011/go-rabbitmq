d1592fe1254b69396aa7738a5f45935e5a21147f
mayur2011 <er.mayur2011@gmail.com>
2022-08-20 01:42:30 +0530

    rmq code setup

---
+++ go.mod
@@ -0,0 +1,8 @@
+module go-rabbitmq
+
+go 1.16
+
+require (
+	github.com/rabbitmq/amqp091-go v1.4.0 // indirect
+	github.com/spf13/viper v1.12.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+func main(){
+	fmt.Println("rmq implementation")
+	// try to connect to rabbitmq
+
+
+	// start listening for messages
+
+
+	// create consumer -- which consumes msg from the queue
+
+
+	// watch the queue and consume events from the topic
+}
