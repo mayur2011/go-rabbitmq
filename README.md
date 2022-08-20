7e48a6cf9c849fd841ec72307ca14afa4dbd7ca3
mayur2011 <er.mayur2011@gmail.com>
2022-08-20 17:19:43 +0530

    adding rmq stubs

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # go-rabbitmq
 Golang implementation for messaging service using Rabbit MQ where Publisher and Subscriber component is added.
+
+// steps involved
+start listening for messages
+create consumer -- which consumes msg from the queue
+watch the queue and consume events from the topic
+
+/*
+NOTES:
+-----
+- write some funcs that allow us to interact with the queue
+- put rmq related func & code in their own package
+- broker is going to have to push something onto the queue
+- now this listener service right now at least is not going to put anything onto the queue
+- instead, the queue will push to this service which listens for things
+*/
--- main.go
+++ main.go
@@ -4,8 +4,18 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
+type Config struct {
+	Rabbit *amqp.Connection
+}
+
+type MessagePayload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
+}
+
 func main(){
-	fmt.Println("rmq implementation")
+	fmt.Println("starting...")	
+	
 	// try to connect to rabbitmq
 	rabbitConn, err := connect()
 	if err !=nil{
@@ -14,16 +24,16 @@ func main(){
 	}
 	defer rabbitConn.Close()
 
+	app := Config{
+		Rabbit: rabbitConn,
+	}
+
 	// start listening for messages
-
-
 	// create consumer -- which consumes msg from the queue
-
-
 	// watch the queue and consume events from the topic
 }
 
-func connect() (*amqp.Connection, error){
+func connect() (*amqp.Connection, error) {
 	var counts int64
 	var backoff = 1*time.Second
 	var connection *amqp.Connection
@@ -31,24 +41,22 @@ func connect() (*amqp.Connection, error){
 	// don't continue until rabbit is ready
 	for {
 		c, err := amqp.Dial("amqp://guest:guest@localhost")
-		if err != nil{
+		if err != nil {
 			panic(err)
 			counts++
 		}else{
 			connection = c
 		}
 
-		if counts > 5{
+		if counts > 5 {
 			fmt.Println(err)
 			return nil, err
 		}
 
-		// 
 		backoff = time.Duration(math.Pow(float64(counts), 2)) * time.Second
 		log.Println("backing off...")
 		time.Sleep(backoff)
 		continue
 	}
-
 	return connection, nil
 }
